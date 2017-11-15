f56e18b8a68950ad6faed76fbd24fd9cf100568b
Juraci Vieira <juraci.vieira@gmail.com>
2017-11-14 22:23:36 -0200

    shameless green

---
+++ spec/bottles_spec.rb
@@ -0,0 +1,82 @@
+require_relative '../lib/bottles'
+require_relative './entire_song'
+
+describe '99 Bottles' do
+  describe '#verse' do
+    it 'returns the 99th verse' do
+      expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+        "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+
+      expect(::Bottles.new.verse(99)).to eq(expected)
+    end
+
+    it 'returns the 89th verse' do
+      expected = "89 bottles of beer on the wall, 89 bottles of beer.\n" +
+        "Take one down and pass it around, 88 bottles of beer on the wall.\n"
+
+      expect(::Bottles.new.verse(89)).to eq(expected)
+    end
+
+    it 'returns the 3th verse' do
+      expected = "3 bottles of beer on the wall, 3 bottles of beer.\n" +
+        "Take one down and pass it around, 2 bottles of beer on the wall.\n"
+
+      expect(::Bottles.new.verse(3)).to eq(expected)
+    end
+
+    it 'returns the 2nd verse' do
+      expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+
+      expect(::Bottles.new.verse(2)).to eq(expected)
+    end
+
+    it 'returns the 1st verse' do
+      expected = "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+        "Take it down and pass it around, no more bottles of beer on the wall.\n"
+
+      expect(::Bottles.new.verse(1)).to eq(expected)
+    end
+
+    it 'returns the 0 verse' do
+      expected = "No more bottles of beer on the wall, no more bottles of beer.\n" +
+        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+
+      expect(::Bottles.new.verse(0)).to eq(expected)
+    end
+  end
+
+  describe '#verses' do
+    context 'when asking verses from 99 to 98' do
+      it 'returns the verses' do
+        expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+          "Take one down and pass it around, 98 bottles of beer on the wall.\n" +
+          "98 bottles of beer on the wall, 98 bottles of beer.\n" +
+          "Take one down and pass it around, 97 bottles of beer on the wall.\n"
+
+        expect(::Bottles.new.verses(99, 98)).to eq(expected)
+      end
+    end
+
+    context 'when asking verses from 2 until 0' do
+      it 'returns the verses' do
+        expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+          "Take one down and pass it around, 1 bottle of beer on the wall.\n" +
+          "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+          "Take it down and pass it around, no more bottles of beer on the wall.\n" +
+          "No more bottles of beer on the wall, no more bottles of beer.\n" +
+          "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+
+        expect(::Bottles.new.verses(2, 0)).to eq(expected)
+      end
+    end
+  end
+
+  describe '#song' do
+    it 'returns the whole song' do
+      expected = Song.get
+
+      expect(::Bottles.new.song).to eq(expected)
+    end
+  end
+end
